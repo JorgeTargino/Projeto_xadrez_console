76ba93a48db19dede0b164d0ab357c8cde24d808
Jorge Targino <jorgetargino.dc@gmail.com>
2024-06-18 09:13:53 -0300

    Iniciando projeto

--- Program.cs
+++ Program.cs
@@ -8,12 +8,7 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Posicao P;
-
-            P = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + P);
-            Console.ReadLine();
+            Console.WriteLine("Hello, World! ");
         }
     }
 }
